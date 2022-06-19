94b92a6df09cd4ba62f5c31d0caf7846e6079428
Daria <dashatur100500@gmail.com>
2022-06-19 17:54:50 +0300

    add task 3 mod 4

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        char ch = Character.toLowerCase(character);
+        if(ch=='a' || ch =='o' || ch == 'i' || ch == 'e' || ch == 'u'){
+            System.out.println("Vowel");
+        } else if(ch>122 || ch<97){
+            System.out.println("wrong alphabet!");
+        } else {
+            System.out.println("Consonant");
+        }
+
     }
 }
